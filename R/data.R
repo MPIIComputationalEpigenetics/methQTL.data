f4ee88abb126955eed7c1ef2eb5c4d4464cc2ebc
Michael Scherer <s9mlscer@gmail.com>
2020-07-14 10:17:21 +0200

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,4 @@
+## Data package for the methQTL package ###
+## The dataset used as an example within this package is obtained from the study:
+## Do C, Lang CF, Lin J, Darbary H et al. Mechanisms and Disease Associations of Haplotype-Dependent Allele-Specific DNA Methylation. Am J Hum Genet 2016 May 5;98(5):934-955. PMID: 27153397
+## available from GEO, accession number GSE79144
